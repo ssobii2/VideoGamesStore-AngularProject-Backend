2639310da462e9a2fa042390b68d815ae3d78326
THUNDER-PC\subha <rntvtf@pte.hu>
2022-06-12 21:53:20 +0200

    optimized code

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // Create an Express JS instance
 const express = require('express');
 const cors = require('cors');
-const authRouter = require('./routes/auth.route');
 const productRouter = require('./routes/product.route');
 
 const app = new express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/api', authRouter);
 app.use('/api', productRouter);
 
 app.listen(4300, () => {
